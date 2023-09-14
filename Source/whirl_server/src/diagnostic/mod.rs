094abac2918cdf5559009b255dd19b09c479963b
adebola-xyz <adebolaakomolafe@gmail.com>
2023-09-14 23:04:59 +0100

    Rename language_server to server

---
+++ Source/whirl_server/src/diagnostic/mod.rs
@@ -0,0 +1,50 @@
+use tower_lsp::lsp_types::{Diagnostic, DiagnosticSeverity, Position, Range};
+use whirl_ast::{ScopeManager, Span};
+use whirl_errors::{LexErrorPos, ProgramError};
+use whirl_printer::{stringify_lex_error, stringify_parse_error, stringify_type_error};
+
+/// Convert a program error to a diagnostic.
+pub fn to_diagnostic(scope_manager: &ScopeManager, error: &ProgramError) -> Diagnostic {
+    let (message, range) = match error {
+        ProgramError::LexerError(lex_error) => (
+            stringify_lex_error(&lex_error.error_type),
+            to_range(match lex_error.position {
+                LexErrorPos::Point(pos) => Span::at(pos),
+                LexErrorPos::Span(span) => span,
+            }),
+        ),
+        ProgramError::ParserError(parse_error) => (
+            stringify_parse_error(&parse_error.error_type),
+            to_range(parse_error.span),
+        ),
+        ProgramError::TypeError(type_error) => (
+            stringify_type_error(scope_manager, &type_error._type),
+            to_range(type_error.spans[0]),
+        ),
+    };
+    Diagnostic {
+        range,
+        severity: Some(DiagnosticSeverity::ERROR),
+        code: None,
+        code_description: None,
+        source: Some(format!("whirl")),
+        message,
+        related_information: None,
+        tags: None,
+        data: None,
+    }
+}
+
+/// Convert a span to a range.
+pub fn to_range(span: Span) -> Range {
+    Range {
+        start: Position {
+            line: span.start[0] - 1,
+            character: span.start[1] - 1,
+        },
+        end: Position {
+            line: span.end[0] - 1,
+            character: span.end[1] - 1,
+        },
+    }
+}
---
+++ Source/whirl_server/src/document_manager.rs
@@ -0,0 +1,119 @@
+use std::sync::RwLock;
+
+use tower_lsp::lsp_types::{
+    Diagnostic, DidChangeTextDocumentParams, DidOpenTextDocumentParams, DocumentDiagnosticParams,
+    DocumentDiagnosticReport, FullDocumentDiagnosticReport, HoverParams, Position,
+    RelatedFullDocumentDiagnosticReport, TextDocumentContentChangeEvent, Url,
+};
+use whirl_ast::ASTVisitor;
+use whirl_semantic::Module;
+
+use crate::{
+    diagnostic::to_diagnostic,
+    did_change::ChangeHandler,
+    hover::{HoverFinder, HoverInfo},
+};
+
+#[derive(Debug)]
+pub struct DocumentManager {
+    documents: RwLock<Vec<WhirlDocument>>,
+}
+
+impl DocumentManager {
+    pub fn new() -> Self {
+        DocumentManager {
+            documents: RwLock::new(vec![]),
+        }
+    }
+    /// Add a new document to be tracked.
+    pub fn add_document(&self, params: DidOpenTextDocumentParams) {
+        let file = WhirlDocument {
+            version: 0,
+            uri: params.text_document.uri,
+            module: Module::from(params.text_document.text.as_str()),
+        };
+        self.documents.write().unwrap().push(file);
+    }
+    /// Hover over support.
+    pub fn get_hover_info(&self, params: HoverParams) -> Option<HoverInfo> {
+        let params = params.text_document_position_params;
+        let documents = self.documents.read().unwrap();
+        documents
+            .iter()
+            .find(|d| d.uri == params.text_document.uri)
+            .map(|document| document.get_hover_for_position(params.position))
+            .flatten()
+    }
+    /// Checks if a uri is already being tracked.
+    pub fn has(&self, uri: &Url) -> bool {
+        for file in self.documents.write().unwrap().iter() {
+            if &file.uri == uri {
+                return true;
+            }
+        }
+        false
+    }
+    /// Handles didChange event
+    pub fn handle_change(&self, params: DidChangeTextDocumentParams) {
+        let mut documents = self.documents.write().unwrap();
+        let document = documents
+            .iter_mut()
+            .find(|d| d.uri == params.text_document.uri);
+        if document.is_none() {
+            return;
+        }
+        let document = document.unwrap();
+        document.refresh(params.text_document.version, params.content_changes);
+    }
+    /// Get diagnostics.
+    pub fn get_diagnostics(
+        &self,
+        params: DocumentDiagnosticParams,
+    ) -> Option<DocumentDiagnosticReport> {
+        let mut documents = self.documents.write().unwrap();
+        let document = documents
+            .iter_mut()
+            .find(|d| d.uri == params.text_document.uri);
+        document.map(|doc| {
+            DocumentDiagnosticReport::Full(RelatedFullDocumentDiagnosticReport {
+                related_documents: None,
+                full_document_diagnostic_report: FullDocumentDiagnosticReport {
+                    result_id: None,
+                    items: doc
+                        .module
+                        .program_errors
+                        .iter()
+                        .map(|p| to_diagnostic(&doc.module.scope_manager, p))
+                        .collect::<Vec<Diagnostic>>(),
+                },
+            })
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct WhirlDocument {
+    uri: Url,
+    version: usize,
+    module: Module,
+}
+
+impl WhirlDocument {
+    fn get_hover_for_position(&self, position: Position) -> Option<HoverInfo> {
+        let position = [position.line + 1, position.character];
+        let hover_finder = HoverFinder::with_scope_manager(&self.module.scope_manager);
+        for statement in &self.module.statements {
+            let hover_info = hover_finder.visit_statement(statement, &position);
+            if hover_info.is_some() {
+                return hover_info;
+            }
+        }
+        return None;
+    }
+    fn refresh(&mut self, version: i32, changes: Vec<TextDocumentContentChangeEvent>) {
+        self.version = version as usize;
+
+        let handler = ChangeHandler::from_module(&mut self.module);
+        handler.update(changes);
+    }
+}
