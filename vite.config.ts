9ef8c397b4d2a2df04d15bc5241a2ac3e48e2657
Sefunmi <adebolaakomolafe@gmail.com>
2024-03-23 08:43:08 +0100

    feat: site: add favicon and public folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './source/website/pages',
+  publicDir: '../public',
   build: {
     outDir: '../../../dist',
     emptyOutDir: true,
